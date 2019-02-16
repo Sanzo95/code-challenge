f13ce44ad90b02d8f6affc39cf9c8a2a2acf36cc
rtritto <r.tritto5@studenti.uniba.it>
2019-02-16 19:33:29 +0100

    aggiunti controlli per risultato e ora inizio in dettagli partita

--- src/app/match-detail/match-detail.component.ts
+++ src/app/match-detail/match-detail.component.ts
@@ -14,10 +14,27 @@ export class MatchDetailComponent {
   @Input()
   teams: Team[] = [];
   idTeamSelected: number;
+  date: string;
+  time: string;
 
-  constructor() { }
+  constructor() {
+  }
 
   teamSelected(idTeam: number) {
     this.idTeamSelected = idTeam;
+  }
+
+  toDate(d: string) {
+    let a = d.split('T');
+    let date = a[0].split('-').reverse().join('/');
+    let time = "";
+    let timea = a[1].substr(0, 2);
+    timea === "00" ? time = "??:??" : time = String(Number(timea) + 1).concat(a[1].substr(2, 3));
+    this.date = date;
+    this.time = time;
+  }
+
+  checkResult(res: string) {
+    return res === null ? "?" : res;
   }
 }
